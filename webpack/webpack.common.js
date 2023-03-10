fc5fb1cecff65fe7d3c0b548d5d76cbbfab6d82f
Alex LIGONY <alex.ligony@gmail.com>
2023-03-11 00:56:37 +0100

    Update all dependencies

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -7,40 +7,39 @@ const mode = npmCommand === 'build' ? 'production' : 'development';
 process.env.NODE_ENV = mode;
 
 module.exports = {
-    mode,
-    entry: {
-        app: path.resolve(__dirname, '../src/main.js')
-    },
-    output: {
-        path: path.resolve(__dirname, '../dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                include: path.resolve(__dirname, '../src'),
-                exclude: /node_modules/,
-                use: {
-                    loader: 'esbuild-loader',
-                    options: {
-                        loader: 'jsx',
-                        target: 'es2015'
-                    }
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@component': path.resolve(__dirname, '../src/components/'),
-            '@img': path.resolve(__dirname, '../src/assets/images/')
+  mode,
+  entry: {
+    app: path.resolve(__dirname, '../src/main.js')
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        include: path.resolve(__dirname, '../src'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'esbuild-loader',
+          options: {
+            loader: 'jsx',
+            target: 'es2015'
+          }
         }
-    },
-    cache: {
-        type: 'filesystem',
-        cacheDirectory: path.resolve(__dirname, '../.cache'),
-        name: mode,
-        maxAge: 86_400_000 // 1 day in milliseconds
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, '../src/')
     }
+  },
+  cache: {
+    type: 'filesystem',
+    cacheDirectory: path.resolve(__dirname, '../.cache'),
+    name: mode,
+    maxAge: 86_400_000 // 1 day in milliseconds
+  }
 };
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,5 @@
 const commonConfig = require('./webpack.common');
-const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const { EsbuildPlugin } = require('esbuild-loader');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,83 +7,83 @@ const path = require('path');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = merge(commonConfig, {
-    output: {
-        filename: 'js/[name].[contenthash].js',
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(c|sa|sc)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name].[hash][ext]'
-                }
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[name].[hash][ext]'
-                }
-            }
+  output: {
+    filename: 'js/[name].[contenthash].js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(c|sa|sc)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash].css',
-            chunkFilename: 'css/[id].[contenthash].css'
-        }),
-        new WebpackManifestPlugin({
-            publicPath: ''
-        })
-    ],
-    optimization: {
-        minimize: true,
-        splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: Infinity,
-            minSize: 0,
-            cacheGroups: {
-                reactVendor: {
-                    test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-                    name: 'react-vendor'
-                },
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor'
-                },
-            }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[hash][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+      chunkFilename: 'css/[id].[contenthash].css'
+    }),
+    new WebpackManifestPlugin({
+      publicPath: ''
+    })
+  ],
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        reactVendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'react-vendor'
         },
-        minimizer: [
-            new ESBuildMinifyPlugin({
-                target: 'es2015'
-            }),
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, '../public/index.html'),
-                favicon: path.resolve(__dirname, '../public/favicon.ico'),
-                templateParameters: {
-                    description: process.env.META_DESCRIPTION,
-                    title: process.env.APP_TITLE
-                },
-                minify: {
-                    removeAttributeQuotes: true,
-                    removeComments: true,
-                    collapseWhitespace: true
-                }
-            })
-        ]
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor'
+        },
+      }
     },
-    cache: {
-        compression: 'brotli',
-        maxAge: 604_800_000  // 1 week in milliseconds
-    }
+    minimizer: [
+      new EsbuildPlugin({
+        target: 'es2015'
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, '../public/index.html'),
+        favicon: path.resolve(__dirname, '../public/favicon.ico'),
+        templateParameters: {
+          description: process.env.META_DESCRIPTION,
+          title: process.env.APP_TITLE
+        },
+        minify: {
+          removeAttributeQuotes: true,
+          removeComments: true,
+          collapseWhitespace: true
+        }
+      })
+    ]
+  },
+  cache: {
+    compression: 'brotli',
+    maxAge: 604_800_000  // 1 week in milliseconds
+  }
 });
